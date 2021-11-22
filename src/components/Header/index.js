15e0a9174ae69f1d074be4c6cfae7f53e4567768
CathyAkoth <catherinececilia22@gmail.com>
2021-11-22 10:11:03 -0800

    Added humburger styled

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -52,6 +52,51 @@ a{
   }
 
 `;
+const HamburgerBtn = styled.button`
+position: relative;
+background-color:transparent;
+width:2rem;
+height:2px;
+cursor:pointer;
+
+&::before,
+::after{
+  content:"";
+  background-color:var(--white);
+  width:2rem;
+  height:2px;
+  display:inline-block;
+  position:absolute;
+  left:0;
+  cursor: pointer;
+  transition: all 0.3s;
+}
+&::before{
+  top: -0.5rem;
+  transform: rotate(0);
+}
+&::after{
+  top: 0.5rem;
+  transform: rotate(0);
+}
+`;
+
+const Button = styled.button`
+background-color: var(--purple);
+padding: 0.5rem 1rem;
+border-radius: 20px;
+color:var(--white);
+font-weight: 600;
+cursor:pointer;
+transition: all 0.2s;
+
+&:hover{
+  transform: scale(1.1);
+}
+&:focus{
+  transform: scale(0.9);
+}
+`;
 const Header = () => {
   return( 
   
@@ -66,10 +111,12 @@ const Header = () => {
     <a href="#about">About</a>
     <a href="#services">Services</a>
     <a href="#contact">
-      <button>Home</button>
+      <Button>Contact Us</Button>
       </a>
   </Nav>
-
+  <HamburgerBtn>
+    <span />
+  </HamburgerBtn>
   </Headers>
   );
 };
--- src/components/ScrollToTop/index.js
+++ src/components/ScrollToTop/index.js
@@ -1,7 +1,7 @@
 // This is ScrollToTop component, To scroll from anywhere to top
 
 const ScrollToTop = () => {
-  return <div>ScrollToTop Component</div>;
+  return <div></div>;
 };
 
 export default ScrollToTop;
