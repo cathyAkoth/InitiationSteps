2f0994ec5301a1736579b3e3b67c5fe45ab10fc1
CathyAkoth <catherinececilia22@gmail.com>
2021-11-22 06:47:43 -0800

    added styles to home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,8 +7,11 @@ import styled from "styled-components";
 
 
 const Container = styled.div`
-
-`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+`;
 const Home = () => {
   return (
   <Container>
